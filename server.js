0bc9b58e14033e94e34f5071e1eb8cf57ebe4cd1
Wonjong Park <wonjong2@gmail.com>
2022-05-01 13:50:12 -0700

    Created GET '/api/notes' route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+// const db = require('./db/db.json');
 
 const PORT = 3001;
 
@@ -15,9 +16,17 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+app.get('/api/notes', (req, res) => 
+    fs.readFile('./db/db.json', 'utf-8', (error, data) => 
+    error ? res.sendStatus(503) : res.json(data)))
+
 app.get('*', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+// app.post('/api/notes', (req, res) => {
+//     req.
+// });
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
