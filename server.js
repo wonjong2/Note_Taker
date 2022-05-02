a00be1ff431cbb80956487e9c14cecde84ee3f70
Wonjong Park <wonjong2@gmail.com>
2022-05-01 22:29:46 -0700

    Create POST '/api/notes' route

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-// const db = require('./db/db.json');
+const {nanoid} = require('nanoid');
 
 const PORT = 3001;
 
@@ -17,16 +17,49 @@ app.get('/notes', (req, res) =>
 );
 
 app.get('/api/notes', (req, res) => 
-    fs.readFile('./db/db.json', 'utf-8', (error, data) => 
-    error ? res.sendStatus(503) : res.json(data)))
+    fs.readFile(path.join('./db/db.json'), 'utf-8', (error, data) => error ? next(error) : res.json(JSON.parse(data))));
 
 app.get('*', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// app.post('/api/notes', (req, res) => {
-//     req.
-// });
+app.post('/api/notes', (req, res) => {
+    const {title, text} = req.body;
+
+    if(title && text) {
+        fs.readFile(path.join('./db/db.json'), 'utf-8', (error, data) => {
+            if(error) {
+                next(error);
+            }
+            
+            const newNote = {
+                title,
+                text,
+                id: nanoid()
+            };
+
+            let noteList = JSON.parse(data);
+            noteList.push(newNote)
+            console.log(typeof noteList, noteList);
+            const noteListString = JSON.stringify(noteList);
+
+            fs.writeFile('./db/db.json', noteListString, (err) => {
+                if(err) {
+                    next(err);
+                }
+                const response = {
+                    status: 'success',
+                    body: newNote,
+                };
+
+                res.status(201).json(response);
+            });
+        });
+    }
+    else {
+        res.status(500).json('Error in creating a note');
+    }
+});
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
