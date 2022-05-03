e8ae1bbccedc679cad1d4ea866a79a1da82383f8
Wonjong Park <wonjong2@gmail.com>
2022-05-03 15:32:52 -0700

    Seperated '/api/notes' routes into notesRouter.js file

---
+++ routes/notesRouter.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const {nanoid} = require('nanoid');
+const {readFile, readAndAdd, readAndDelete} = require('../helpers/fsUtils');
+
+router.get('/', (req, res) => {
+    console.log(`${req.method} received`);
+    readFile('./db/db.json')
+    .then(data => res.json(JSON.parse(data)))
+    .catch(err => res.status(500).json('Error in reading notes'));
+});
+
+router.post('/', (req, res) => {
+    const {title, text} = req.body;
+    if(title && text) {
+        const newNote = {
+            title,
+            text,
+            id: nanoid()
+        };
+        readAndAdd('./db/db.json', newNote, res);
+    }
+    else {
+        res.status(400).json('Invalid data');
+    }
+});
+
+router.delete('/:id', (req, res) => {
+    const reqId = req.params.id;
+    readAndDelete('./db/db.json', reqId, res);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
-// const fs = require('fs');
-const {nanoid} = require('nanoid');
 const {readFile, readAndAdd, readAndDelete} = require('./helpers/fsUtils');
+const notesRouter = require('./routes/notesRouter');
 
 const PORT = 3001;
 
@@ -13,40 +12,15 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(express.static('public'));
 
+app.use('/api/notes', notesRouter);
+
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.get('/api/notes', (req, res) => {
-    console.log(`${req.method} received`);
-    readFile('./db/db.json')
-    .then(data => res.json(JSON.parse(data)))
-    .catch(err => res.status(500).json('Error in reading notes'));
-});
-
 app.get('*', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-
-app.post('/api/notes', (req, res) => {
-    const {title, text} = req.body;
-    if(title && text) {
-        const newNote = {
-            title,
-            text,
-            id: nanoid()
-        };
-        readAndAdd('./db/db.json', newNote, res);
-    }
-    else {
-        res.status(400).json('Invalid data');
-    }
-});
-
-app.delete('/api/notes/:id', (req, res) => {
-    const reqId = req.params.id;
-    readAndDelete('./db/db.json', reqId, res);
-});
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
